00bcbde95f7ae2c5bd30d26047af10b9d4d50f0b
Nich Headlong <nichheadlong@googlemail.com>
2019-05-25 16:17:41 -0700

    removed internal server

---
+++ src/.next/static/chunks/styles.js
@@ -0,0 +1,29 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+
+/***/ "./assets/css/styles.scss":
+/*!********************************!*\
+  !*** ./assets/css/styles.scss ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by extract-css-chunks-webpack-plugin
+    if(true) {
+      // 1558826178735
+      var cssReload = __webpack_require__(/*! ../../node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js */ "./node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js")(module.i, {"fileMap":"{fileName}"});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);;
+    }
+  
+    if(true) {
+      // 1558826178735
+      var cssReload = __webpack_require__(/*! ../../node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js */ "./node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js")(module.i, {"fileMap":"{fileName}"});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);;
+    }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=styles.js.map
---
+++ src/.next/static/webpack/styles.f6c62b090789c6d5d298.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("styles",{
+
+/***/ "":
+false,
+
+/***/ "./assets/css/styles.scss":
+/*!********************************!*\
+  !*** ./assets/css/styles.scss ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by extract-css-chunks-webpack-plugin
+    if(true) {
+      // 1558826178735
+      var cssReload = __webpack_require__(/*! ../../node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js */ "./node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js")(module.i, {"fileMap":"{fileName}"});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);;
+    }
+  
+    if(true) {
+      // 1558826178735
+      var cssReload = __webpack_require__(/*! ../../node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js */ "./node_modules/extract-css-chunks-webpack-plugin/dist/hotModuleReplacement.js")(module.i, {"fileMap":"{fileName}"});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);;
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=styles.f6c62b090789c6d5d298.hot-update.js.map
